a4a89ad6999ba064579c1efdd94b9be5b2b7c69d
Gurpreet Singh <gurpreetr1234@gmail.com>
2024-11-18 11:32:53 +0530

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl'>
+      hi this is runner game
+      </div>
+      
+    </>
+  )
+}
+
+export default App
