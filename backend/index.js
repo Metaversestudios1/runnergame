39a8861df8e7bd00eb06f9e885f1c108e20050d6
nikita <nikita@gmail.com>
2025-01-06 18:02:19 +0530

    exceed file added

--- backend/index.js
+++ backend/index.js
@@ -51,8 +51,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.json({ limit: '2gb' }));
+app.use(express.urlencoded({ limit: '2gb', extended: true }));
 
 // Serve static files
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
